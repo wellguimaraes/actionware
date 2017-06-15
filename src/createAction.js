a3b48457300f222467f4273a5db4f6669f3256bc
Wellington Guimaraes <wellguimaraes@gmail.com>
2017-06-15 15:47:14 -0400

    improved smartAction naming

--- src/createAction.js
+++ src/createAction.js
@@ -8,77 +8,81 @@ import { getStore } from './';
 export default function(action) {
   if (action.hasOwnProperty('_actionware')) return action._actionware;
 
-  const actionName    = action.name;
+  const actionName = action.name;
   const generatedName = getActionName(prefix, actionName, action);
   const successAction = generatedName;
-  const errorAction   = `${generatedName}_error`;
+  const errorAction = `${generatedName}_error`;
   const loadingAction = `${generatedName}_loading`;
 
-  const smartAction = function() {
-    const args = arguments;
+  const smartAction = {
+    [actionName]: function() {
+      const args = arguments;
 
-    let store = getStore();
+      let store = getStore();
 
-    const { dispatch } = store;
+      const { dispatch } = store;
 
-    const handleError = error => {
-      // call action error handler if available
-      if (action.onError) action.onError({ args, error });
+      const handleError = error => {
+        // call action error handler if available
+        if (action.onError) action.onError({ args, error });
 
-      // call global error listeners
-      errorListeners.forEach(fn => fn({ action: smartAction, args, error }));
+        // call global error listeners
+        errorListeners.forEach(fn => fn({ action: smartAction, args, error }));
 
-      // dispatch error action
-      dispatch({ type: errorAction, payload: { error, args } });
-    };
+        // dispatch error action
+        dispatch({ type: errorAction, payload: { error, args } });
+      };
 
-    try {
-      // call global loading listeners
-      if (action)
-        loadingListeners.forEach(fn => fn({ action: smartAction, args }));
+      try {
+        // call global loading listeners
+        if (action)
+          loadingListeners.forEach(fn => fn({ action: smartAction, args }));
 
-      // dispatch loading action
-      if (action) dispatch({ type: loadingAction, payload: true });
+        // dispatch loading action
+        if (action) dispatch({ type: loadingAction, payload: true });
 
-      const actionResponse  = action && action.apply(action, [ ...args, store ]);
-      const responsePromise = Promise.resolve(actionResponse);
+        const actionResponse = action && action.apply(action, [...args, store]);
+        const responsePromise = Promise.resolve(actionResponse);
 
-      return responsePromise.then(
-        payload => {
-          // dispatch success actions
-          dispatch({ type: successAction, payload });
+        return responsePromise.then(
+          payload => {
+            // dispatch success actions
+            dispatch({ type: successAction, payload });
 
-          if (typeof action.onSuccess === 'function')
-            action.onSuccess.call(null, { payload, args, store });
+            if (typeof action.onSuccess === 'function')
+              action.onSuccess.call(null, { payload, args, store });
 
-          // call global success listeners
-          successListeners.forEach(fn =>
-            fn({ action: smartAction, args, payload }),
-          );
-        },
-        error => {
-          handleError(error);
-          return Promise.reject(error);
-        },
-      );
-    } catch (error) {
-      handleError(error);
-      throw error;
+            // call global success listeners
+            successListeners.forEach(fn =>
+              fn({ action: smartAction, args, payload })
+            );
+          },
+          error => {
+            handleError(error);
+            return Promise.reject(error);
+          }
+        );
+      } catch (error) {
+        handleError(error);
+        throw error;
+      }
     }
   };
 
-  action.toString    = () => successAction;
-  action.success     = successAction;
-  action.error       = errorAction;
-  action.loading     = loadingAction;
-  action._actionware = smartAction;
+  const namedSmartAction = smartAction[actionName];
 
-  smartAction.toString   = () => successAction;
-  smartAction.success    = successAction;
-  smartAction.error      = errorAction;
-  smartAction.loading    = loadingAction;
-  smartAction.actionName = actionName;
-  smartAction.type       = 'actionware';
+  action.toString = () => successAction;
+  action.success = successAction;
+  action.error = errorAction;
+  action.loading = loadingAction;
+  action._actionware = namedSmartAction;
 
-  return smartAction;
+  namedSmartAction.toString = () => successAction;
+  namedSmartAction.success = successAction;
+  namedSmartAction.error = errorAction;
+  namedSmartAction.loading = loadingAction;
+  namedSmartAction.actionName = actionName;
+  namedSmartAction.type = 'actionware';
+
+  return namedSmartAction;
 }
